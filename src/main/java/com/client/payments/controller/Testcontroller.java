ab96cd598b4679858d562214786c2f10eeca753b
israr husain <israrhusain5892@gmail.com>
2023-09-17 11:28:00 +0530

    mm

---
+++ src/main/java/com/client/payments/controller/Testcontroller.java
@@ -0,0 +1,13 @@
+package com.client.payments.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testcontroller {
+
+	@GetMapping("/controller")
+	public String Test() {
+		return "welcome to spring boot";
+	}
+}
